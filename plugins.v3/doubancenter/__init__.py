13c5fb4b5090d9df77ba8c15a56d84e8cb6399ba
Kurisu <z.y.biubiu@gmail.com>
2026-08-27 22:45:11 +0800

    fix(doubancenter): 补齐本地开发版号

--- plugins.v3/doubancenter/__init__.py
+++ plugins.v3/doubancenter/__init__.py
@@ -1,5 +1,5 @@
 """
-DoubanCenter v3.0.1 - MoviePilot V3 本地插件
+DoubanCenter v3.0.2 - MoviePilot V3 本地插件
 整合：榜单订阅 + 豆瓣时间 + 仪表盘双面板
 """
 import threading
@@ -34,7 +34,7 @@ class DoubanCenter(_PluginBase):
     plugin_desc = "豆瓣榜单订阅 + 豆瓣时间 + 仪表盘，一站式豆瓣集成。"
     plugin_icon = "douban.png"
     plugin_color = "#2E7D32"
-    plugin_version = "3.0.1"
+    plugin_version = "3.0.2"
     plugin_author = "Kurisu"
     author_url = "https://github.com/z2561221"
     plugin_config_prefix = "doubancenter_"
