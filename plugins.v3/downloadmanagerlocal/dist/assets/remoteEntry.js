a42d64473b398ea4cfb43a3dbbcd905cf3da9f9e
Kurisu <z.y.biubiu@gmail.com>
2026-09-13 09:34:42 +0800

    fix(downloadmanagerlocal): 构建清理跟随实际输出目录

--- plugins.v3/downloadmanagerlocal/dist/assets/remoteEntry.js
+++ plugins.v3/downloadmanagerlocal/dist/assets/remoteEntry.js
@@ -2,11 +2,11 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./Config":()=>{
-      dynamicLoadingCss(["__federation_expose_Config-C-WnPIq9.css"], false, './Config');
-      return __federation_import('./__federation_expose_Config-CjrGJVmp.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
+      dynamicLoadingCss(["__federation_expose_Config-C7Ycoi7O.css"], false, './Config');
+      return __federation_import('./__federation_expose_Config-BtSzfb3R.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
 "./Page":()=>{
-      dynamicLoadingCss(["__federation_expose_Page-D1Fuor4m.css"], false, './Page');
-      return __federation_import('./__federation_expose_Page-L-ts2tfF.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["__federation_expose_Page-DTv3P6B6.css"], false, './Page');
+      return __federation_import('./__federation_expose_Page-Dz69DLNQ.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
